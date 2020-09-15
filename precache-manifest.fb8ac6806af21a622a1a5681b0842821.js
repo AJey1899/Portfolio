7aa2ebd6638a12d944414d36ff319fe77196d49f
unknown <18adityajadhav@gmail.com>
2020-09-15 18:13:25 +0530

    Updates

---
+++ precache-manifest.fb8ac6806af21a622a1a5681b0842821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd7c6ec1824194df5444d0b8d01666e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e51d88f633ea56de651c",
+    "url": "/Portfolio/static/css/main.74615b24.chunk.css"
+  },
+  {
+    "revision": "62be7184e84f73bf3bee",
+    "url": "/Portfolio/static/js/2.7d8cc956.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.7d8cc956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51d88f633ea56de651c",
+    "url": "/Portfolio/static/js/main.b519c12f.chunk.js"
+  },
+  {
+    "revision": "d680954d34fbac82dab0",
+    "url": "/Portfolio/static/js/runtime-main.2720a3ae.js"
+  }
+]);
