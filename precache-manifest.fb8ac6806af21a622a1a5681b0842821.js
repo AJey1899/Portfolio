2f296203ea0579fd7dd61380bf60f7c4297eeb23
unknown <18adityajadhav@gmail.com>
2020-09-15 18:25:07 +0530

    Updates

--- precache-manifest.fb8ac6806af21a622a1a5681b0842821.js
+++ precache-manifest.fb8ac6806af21a622a1a5681b0842821.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "074b91dcf8e81b429eef3d1002dca013",
+    "revision": "cbd7c6ec1824194df5444d0b8d01666e",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "458f26096fff302091e8",
+    "revision": "e51d88f633ea56de651c",
     "url": "/Portfolio/static/css/main.74615b24.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/js/2.7d8cc956.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "458f26096fff302091e8",
-    "url": "/Portfolio/static/js/main.386b92c6.chunk.js"
+    "revision": "e51d88f633ea56de651c",
+    "url": "/Portfolio/static/js/main.b519c12f.chunk.js"
   },
   {
     "revision": "d680954d34fbac82dab0",
